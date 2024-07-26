ca0d0ab1571364b9e9495bcd17aa6e5cf12b0cec
Nhims00 <sown29042005@gmail.com>
2024-07-27 00:32:07 +0700

    up

--- Assets/Script/ChonLV.cs
+++ Assets/Script/ChonLV.cs
@@ -1,21 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Tilemaps;
 using UnityEngine;
 
 public class ChonLV : MonoBehaviour
 {
-    public GameObject PanelMenu, ButtonM1, ButtonM2, ButtonM3, Text;
+    public GameObject Map, ManHinhChinh, hienthi;
+    
     // Start is called before the first frame update
     void Start()
     {
-        PanelMenu.SetActive(true);
+       
     }
 
-    // Update is called once per frame
+    public void Bat()
+    {
+        Map.SetActive(!Map.activeSelf);
+        ManHinhChinh.SetActive(false);
+        hienthi.SetActive(false);
+    }
+
+    public void exit()
+    {
+        Map.SetActive(!Map.activeSelf);
+        ManHinhChinh.SetActive(true);
+        hienthi.SetActive(true);
+    }
     void Update()
     {
        
-        ButtonM1.SetActive(true); 
+      
         
     }
 }
